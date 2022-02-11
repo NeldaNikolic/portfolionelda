e35fa724fec4b4c11293f30ce9ec5e39af3f7433
NeldaNikolic <neldajeanbaptiste130@gmail.com>
2022-02-11 15:28:15 -0500

    Pusihng modified files that were not pushed previously

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
 
+import "./App.css";
+import Home from "./portfoliocomtainer/home/Home";
+import Portfolio from "./portfoliocomtainer/portfolio/Portfolio";
+import Footer from "./portfoliocomtainer/footer/Footer";
+import NavBar from "./portfoliocomtainer/navBar/NavBar";
+import About from "./portfoliocomtainer/about/About";
+import Contact from "./portfoliocomtainer/contact/FormSingUp";
+import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Routes>
+          <Route path="/" element={<><Home /><About /> <Portfolio /> <Contact /></>} />
+          <Route path="about" element={<About />} />
+          <Route path="Portfolio" element={<Portfolio />} />
+          <Route path="Contact" element={<Contact />} />
+          <Route path="*" element={<><Home /></>} />
+        </Routes>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
